a84c2367880e66b41f5019590146c21355ded60a
NameSteven <876130540@qq.com>
2018-08-29 16:20:55 +0800

    项目初始提交
    项目初始提交

---
+++ cloudbuildtest/Assets/Script/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Test : MonoBehaviour {
+
+   public   Text m_Text;
+   public   Button m_Button;
+    int a = 0;
+	// Use this for initialization
+	void Start () {
+        m_Button.onClick.AddListener(new UnityEngine.Events.UnityAction(()=>
+        {
+            a++;
+            a++;
+            m_Text.text = a.ToString();
+        }));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
